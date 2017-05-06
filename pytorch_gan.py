5866845c61f142612cd468020d50868eac2dbb77
champ <amritansh2007@gmail.com>
2017-05-05 21:00:44 -0600

    Generator Class added
    added Generator class with constructor to intialize NN layers and
    forward connections.

--- pytorch_gan.py
+++ pytorch_gan.py
@@ -1,4 +1,4 @@
-# GAN using pytorch...!! Hurray!!
+# GAN using pytorch...!! 
 
 # amritansh
 
@@ -6,6 +6,10 @@ import argparse
 import numpy as np
 # from scipy.stats import norm
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import seaborn as sns
@@ -35,4 +39,19 @@ class GeneratorDistribution(object):
     def sample(self, N):
         return np.linspace(-self.range, self.range, N) + \
             np.random.random(N) * 0.01
+
+class Generator(nn.Module):
+	def __init__(self, input, hidden, output):
+		super(Generator, self).__init__()
+        self.map_01 = nn.Linear(input, hidden)
+        self.map_02 = nn.Linear(hidden, hidden)
+		self.map_03 = nn.Linear(hidden, output)
+	
+	def forward(self,x):
+		x = F.elu(self.map_01(x))
+		x = F.sigmoid(self.map_02(x))
+		return self.map_03(x)
+
+
+
 
