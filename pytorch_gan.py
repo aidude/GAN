96b89f3d1507456f14fe722cf741eda8f284f294
champ <amritansh2007@gmail.com>
2017-05-06 22:36:52 -0600

    Added parameters for the model and data

--- pytorch_gan.py
+++ pytorch_gan.py
@@ -12,15 +12,32 @@ import torch.optim as optim
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 from matplotlib import animation
-import seaborn as sns
 
-# sns.set(color_codes=True)
 
-# seed = 42
-# np.random.seed(seed)
 
 (name, preprocess, d_input_func) = ("Data and variances", lambda data: decorate_with_diffs(data, 2.0), lambda x: x * 2)
 print("Using data [%s]" % (name))
+
+
+class Parameters(object):
+	data_mean = 4.0
+	data_std_dev = 2.0
+	gen_input_size = 1     # Random noise dimension coming into generator, per output vector
+	gen_hidden_size = 50   # Generator complexity
+	gen_output_size = 1    # size of generated output vector
+	dis_input_size = 100   # Minibatch size - cardinality of distributions
+	dis_hidden_size = 50   # Discriminator complexity
+	dis_output_size = 1    # Single dimension for 'real' vs. 'fake'
+	mini_batch_size = dis_input_size
+
+	dis_learning_rate = 2e-4  
+	gen_learning_rate = 2e-4
+	optimum_betas = (0.9, 0.999)
+	num_epochs = 30000
+	print_interval = 500
+
+	k_steps = 1  # 'k' steps in the original GAN paper. Can put the discriminator on higher training freq than generator
+	g_steps = 1
 
 
 class DataDistribution(object):
@@ -62,5 +79,11 @@ class Discriminator(nn.Module):
         x = F.elu(self.map_02(x))
 		return F.sigmoid(self.map_03(x))
 
+
+def extract_data():
+	return v.data.storage().tolist()
+
+def stats(d):
+	return [np.mean(d), np.std(d)]
 
 def model():
