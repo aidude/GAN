805a5696080162521f70a5487641476118614cba
champ <amritansh2007@gmail.com>
2017-05-08 23:54:05 -0600

    Added code to run in epochs
    Code for training on real and fake data

--- pytorch_gan.py
+++ pytorch_gan.py
@@ -101,6 +101,27 @@ criterion = nn.BCELoss()  # Binary cross entropy: http://pytorch.org/docs/nn.htm
 d_optimizer = optim.Adam(D.parameters(), lr=d_learning_rate, betas=optim_betas)
 g_optimizer = optim.Adam(G.parameters(), lr=g_learning_rate, betas=optim_betas)
 
+
+
+for epoch in range(num_epochs):
+    for d_index in range(d_steps):
+        # 1. Train D on real+fake
+        D.zero_grad()
+
+        #  1A: Train D on real
+        d_real_data = Variable(d_sampler(d_input_size))
+        d_real_decision = D(preprocess(d_real_data))
+        d_real_error = criterion(d_real_decision, Variable(torch.ones(1)))  # ones = true
+        d_real_error.backward() # compute/store gradients, but don't change params
+
+        #  1B: Train D on fake
+        d_gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
+        d_fake_data = G(d_gen_input).detach()  # detach to avoid training G on these labels
+        d_fake_decision = D(preprocess(d_fake_data.t()))
+        d_fake_error = criterion(d_fake_decision, Variable(torch.zeros(1)))  # zeros = fake
+        d_fake_error.backward()
+		d_optimizer.step() # Only optimizes D's parameters; changes based on stored gradients from backward()
+
 # def model():
 
 
